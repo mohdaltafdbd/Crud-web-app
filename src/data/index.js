1d1ef9f0eec87a67976c3bf4ec9ea69925c97ac5
mohdaltafdbd <mohdaltafdbd@gmail.com>
2022-09-29 12:12:02 +0530

    Deployed website

---
+++ src/data/index.js
@@ -0,0 +1,37 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Altaf',
+    lastName: 'Ziya',
+    email: 'altafziya@example.com',
+    salary: '15000',
+    date: '2019-04-11',
+  },
+  {
+    id: 2,
+    firstName: 'Aakash',
+    lastName: 'Gupta',
+    email: 'aakash@gmial.com',
+    salary: '80000',
+    date: '2019-04-17',
+  },
+  {
+    id: 3,
+    firstName: 'Mujassim',
+    lastName: 'Gufran',
+    email: 'mujassim@example.com',
+    salary: '79000',
+    date: '2019-05-01',
+  },
+  {
+    id: 4,
+    firstName: 'Roky',
+    lastName: 'Roky',
+    email: 'roky@example.com',
+    salary: '16000',
+    date: '2019-05-03',
+  },
+  
+];
+
+export { employeesData };
